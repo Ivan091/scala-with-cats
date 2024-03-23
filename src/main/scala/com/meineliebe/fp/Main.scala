cb659db42c4e5b4c602833099425514b9d6d4165
Ivan Kornasevich <kornasevich@gmail.com>
2024-03-24 00:47:57 +0100

    show

--- src/main/scala/com/meineliebe/fp/Main.scala
+++ src/main/scala/com/meineliebe/fp/Main.scala
@@ -1,42 +1,8 @@
 package com.meineliebe.fp
-import com.meineliebe.fp.PrintableInstances.given
+import cats.Show
 
 final case class Cat(name: String, age: Int, color: String)
 
 object Cat {
-  given Printable[Cat] with {
-    override def format(cat: Cat): String = {
-      val name = Printable.format(cat.name)
-      val age = Printable.format(cat.age)
-      val color = Printable.format(cat.color)
-      s"$name is a $age year-old $color cat."
-    }
-  }
-}
-
-trait Printable[A] {
-  def format(x: A): String
-}
-
-object Printable {
-  def format[A](x: A)(using P: Printable[A]): String = P.format(x)
-
-  def print[A](x: A)(using P: Printable[A]): Unit = {
-    println(P.format(x))
-  }
-}
-
-object PrintableInstances {
-  given Printable[Int] with {
-    override def format(x: Int): String = x.toString
-  }
-
-  given Printable[String] with {
-    override def format(x: String): String = x
-  }
-}
-
-extension [A](x: A) {
-  def format(using P: Printable[A]): String = Printable.format(x)
-  def print(using P: Printable[A]): Unit = Printable.print(x)
+  given Show[Cat] = Show.show[Cat]{cat => cat.toString}
 }
--- src/test/scala/com/meineliebe/fp/MainTest.scala
+++ src/test/scala/com/meineliebe/fp/MainTest.scala
@@ -1,13 +1,15 @@
 package com.meineliebe.fp
 
+import cats.syntax.all.toShow
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 final class MainTest extends AnyFreeSpec with Matchers {
 
   "Main" in {
-    val cat = Cat("Garfield", 41, "ginger and black")
-    cat.print
-    Printable.print(cat)
+    val cat = Cat("a", 2, "c")
+    println {
+      cat.show
+    }
   }
 }
