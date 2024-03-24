4120898f38fa6766a5199d4a3e433a843306075d
Ivan Kornasevich <kornasevich@gmail.com>
2024-03-24 01:13:29 +0100

    monoid

--- src/main/scala/com/meineliebe/fp/Main.scala
+++ src/main/scala/com/meineliebe/fp/Main.scala
@@ -1,8 +1,4 @@
 package com.meineliebe.fp
-import cats.Show
 
-final case class Cat(name: String, age: Int, color: String)
+object Main
 
-object Cat {
-  given Show[Cat] = Show.show[Cat]{cat => cat.toString}
-}
--- src/test/scala/com/meineliebe/fp/MainTest.scala
+++ src/test/scala/com/meineliebe/fp/MainTest.scala
@@ -1,15 +1,43 @@
 package com.meineliebe.fp
 
-import cats.syntax.all.toShow
+import cats.implicits.catsSyntaxSemigroup
+import cats.kernel.{Monoid, Semigroup}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
+import cats.syntax.all.toFoldableOps
+
+object MainTest {
+  given Monoid[Int] with {
+    override def combine(x: Int, y: Int): Int = x + y
+    override def empty: Int = 0
+  }
+
+  given[A](using M: Monoid[A]): Monoid[Option[A]] with {
+    override def combine(x: Option[A], y: Option[A]): Option[A] = {
+      x match {
+        case Some(v1) => y match {
+          case Some(v2) => Option(v1 |+| v2)
+          case None => x
+        }
+        case None => y match {
+          case Some(value) => Option(value)
+          case None => Option.empty
+        }
+      }
+
+    }
+
+    override def empty: Option[A] = Option.empty
+  }
+
+
+}
 
 final class MainTest extends AnyFreeSpec with Matchers {
 
   "Main" in {
-    val cat = Cat("a", 2, "c")
     println {
-      cat.show
+      List.range(1, 10).map(Option.apply).combineAll
     }
   }
 }
